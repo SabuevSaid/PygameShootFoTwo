59b495d2de800e443639b6b171b1abadd78e4f26
Said <sksabuev@gmail.com>
2024-03-06 08:38:01 +0300

    add direct

--- config.py
+++ config.py
@@ -13,9 +13,22 @@ def draw(obj):
      window.blit(obj.texture, (obj.w, obj.h))
 def output_on_display(s):
      window.blit(text.render(f'{s}', False, (255, 255, 255)), (100, tile * 18))
-player_texture = pygame.image.load('items/mario2.png')
 window = pygame.display.set_mode((tile * 40, tile * 20))
 bg = pygame.image.load('items/nebo.png')
+turns = {'left': [
+     pygame.image.load('items/mario_left2.png'),
+     pygame.image.load('items/mario_left3.png'),
+     pygame.image.load('items/mario_left4.png'),
+     pygame.image.load('items/mario_left5.png'),
+], 'right': [
+     pygame.image.load('items/mario_right2.png'),
+     pygame.image.load('items/mario_right3.png'),
+     pygame.image.load('items/mario_right4.png'),
+     pygame.image.load('items/mario_right5.png'),
+], 'up': [
+     pygame.image.load('items/mario_right_jump.png'),
+     pygame.image.load('items/mario_left_jump.png')
+]}
 play = True
 player_x = tile * 20
 player_y = tile * 10
--- player.py
+++ player.py
@@ -4,13 +4,15 @@ pygame.init()
 class Player:
     def __init__(self, w, h, keyList):
         self.w, self.h = w, h
+        self.direct = 'right'
         self.jump_size = 16
         self.graviti = True
-        self.texture = pygame.image.load('items/mario2.png')
+        self.anim = 0
         self.subjects = config.subjects
         self.keyList = keyList
         self.is_jump = False
         self.on_earth = False
+        self.texture = [0, 0]
         self.graviti_size = 1
         self.up = keyList[0]
         self.right = keyList[1]
@@ -18,23 +20,41 @@ class Player:
         self.rect = pygame.Rect(self.w, self.h, config.tile, config.tile)
         config.subjects.append(self)
     def update(self, keys):
+        self.anim = 0
         if keys[self.left]:
             if not any([pygame.Rect(self.w - 7, self.h, config.tile, config.tile).colliderect(obj.rect) for obj in config.objects]):
                 self.w -= 8
+                self.direct = 'left'
+                if self.anim >= 3:
+                    self.anim = 0
+                else:
+                    self.anim += 1
         elif keys[self.right]:
             if not any([pygame.Rect(self.w + 7, self.h, config.tile, config.tile).colliderect(obj.rect) for obj in config.objects]):
                 self.w += 8
+                self.direct = 'right'
+                if self.anim >= 3:
+                    self.anim = 0
+                else:
+                    self.anim += 1
         if self.on_earth:
             if keys[pygame.K_UP]:
                 self.is_jump = True
                 self.graviti_size = 1
         if self.is_jump:
+            if self.direct == 'right':
+                self.anim = 0
+                self.direct = 'up'
+            else:
+                self.anim = 1
+                self.direct = 'up'
             if self.jump_size > 0:
                 self.graviti = False
             else:
                 self.jump_size = 16
                 self.graviti = True
                 self.is_jump = False
+
             self.jump_size -= 1
         if not any([pygame.Rect(self.w, self.h + self.graviti_size, config.tile, config.tile).colliderect(obj.rect) for obj in config.objects]):
             self.on_earth = False
@@ -46,7 +66,9 @@ class Player:
             self.is_jump = False
             self.graviti_size = 1
         if not self.graviti:
-            self.h -= self.jump_size
+            if not any([pygame.Rect(self.w, self.h - self.jump_size, config.tile, config.tile).colliderect(obj.rect) for obj in config.objects]):
+                self.h -= self.jump_size
     def draw(self):
+        self.texture = config.turns[self.direct][self.anim]
         config.draw(self)
         config.output_on_display(f'wight: {self.w}, height: {self.h}')
